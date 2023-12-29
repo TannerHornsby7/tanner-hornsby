eacba6c8ed95cca0783f51187d04494ec562769a
Tanner Hornsby <thornsby@uchicago.edu>
2023-12-29 00:09:22 -0600

    update theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,29 @@
-// app/layout.tsx
-import { Providers } from './providers'
+import "@/styles/globals.css"
+import { Inter as FontSans } from "next/font/google"
+import Providers from "./providers"
+import Nav from "@/components/ui/nav"
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode,
-}) {
+import { cn } from "@/lib/utils"
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export default function RootLayout({ children }:{ children: React.ReactNode}) {
   return (
-    <html lang='en'>
-      <body>
-        <Providers>{children}</Providers>
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <Providers>
+        <Nav/>
+        {children}
+        </Providers>
       </body>
     </html>
   )
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,10 @@
-// app/providers.tsx
-'use client'
-import '@fontsource/raleway/300.css'
-import '@fontsource/slackey/400.css'
-import '@fontsource/open-sans/700.css'
+import { ThemeProvider } from "@/components/theme-provider"
 
-import theme from '../../theme'
-import { ChakraProvider } from '@chakra-ui/react'
-
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider theme={theme}>{children}</ChakraProvider>
+export default function Providers({ children }: { children: React.ReactNode }) {
+    return <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+        >{children}</ThemeProvider>
 }
